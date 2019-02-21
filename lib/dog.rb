23f2a558a678032e8e5b15313e99034e023aaf9b
github username <github email address>
2019-02-21 11:36:18 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  attr_accessor :name
+
+  def initialize
+    @name = "fido"
+  end
+
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+
+
+
+end
